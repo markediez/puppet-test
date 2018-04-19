7022df32e03576658c1a95f73c82e7a19ce8c479
Mark Diez <mark.diez@pearson.com>
2018-04-18 22:43:22 -0400

    Output hiera message as notification
    RGHT-

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,7 @@
 notify { "Hello from master environment": }
+
+$message = hiera("message")
+$message_specific = hiera("message_specific")
+
+notify { "'message' is ${message}": }
+notify { "'specific_message' is ${specific_message}'": }
